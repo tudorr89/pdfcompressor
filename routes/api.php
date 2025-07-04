0c85dc75e11b4909d25cf8902171de2917361e67
Tudor Rusu <tudorr89@gmail.com>
2025-07-04 14:49:58 +0300

    add static page

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\API\PdfController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/pdfs/upload', [PdfController::class, 'upload']);
-Route::get('/pdfs/{id}/status', [PdfController::class, 'status']);
+Route::post('/pdfs/upload', [PdfController::class, 'upload'])->name('pdfs.upload');
+Route::get('/pdfs/{id}/status', [PdfController::class, 'status'])->name('pdfs.status');
 Route::get('/pdfs/download/{id}/{type?}', [PdfController::class, 'download'])->name('pdfs.download');
-Route::get('/pdfs', [PdfController::class, 'index']);
